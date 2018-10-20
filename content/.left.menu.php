a8774d535d7522db0fdb92d771c2e605c5f26b16
kiloman-76 <enkiluluka@gmail.com>
2018-10-20 07:49:37 +0300

    Addnig a new Bitrix project

---
+++ content/.left.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Новости", 
+		"/content/news/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Доска объявлений", 
+		"/content/board/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Каталог ресурсов", 
+		"/content/links/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Часто задаваемые вопросы", 
+		"/content/faq/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ e-store/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Каталог книг", 
+		"/e-store/books/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Аффилиаты", 
+		"/e-store/affiliates/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Платный контент", 
+		"/e-store/paid/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
