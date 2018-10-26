173e43ba0b8778add677a322d428a2dc4d2fa059
kiloman-76 <enkiluluka@gmail.com>
2018-10-26 13:52:55 +0300

    404 page added

--- 404.php
+++ 404.php
@@ -7,14 +7,7 @@ CHTTP::SetStatus("404 Not Found");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
-
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", Array(
-	"LEVEL"	=>	"3",
-	"COL_NUM"	=>	"2",
-	"SHOW_DESCRIPTION"	=>	"Y",
-	"SET_TITLE"	=>	"Y",
-	"CACHE_TIME"	=>	"3600"
-	)
-);
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<h2>Страница не найдена</h2>
+	<a href="/test/">Перейти на главную</a>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
